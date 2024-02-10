8ddb23654c55ef8c215043b753ebbab72bbd0074
ShivanshKansal19 <shivanshkansal19@gmail.com>
2024-02-11 01:28:28 +0530

    User login system added
    omgomgomgomg
    dimag kharab ho gaya isme

--- base/urls.py
+++ base/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('location/', views.location, name='location'),
+    path('login/', views.loginUser, name='login'),
+    path('logout/', views.logoutUser, name='logout'),
+    path('signup/', views.registerUser, name='signup'),
+
 ]
