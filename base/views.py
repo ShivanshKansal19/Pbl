ed8e1e5ae239f0e9f8749247ab2194f4a0c97368
ShivanshKansal19 <shivanshkansal19@gmail.com>
2024-02-11 16:52:45 +0530

    Login system updated
    Added remember me feature

--- base/views.py
+++ base/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
+from django.conf import settings
 
 # Create your views here.
 
@@ -12,11 +12,14 @@ from django.contrib.auth.forms import UserCreationForm
 def loginUser(request):
     page = 'login'
     if request.user.is_authenticated:
+        print(request.session.get_expiry_date)
         return redirect('home')
 
     if request.method == 'POST':
+        valuenext = request.POST.get('next')
         username = request.POST.get('username')
         password = request.POST.get('password')
+        remember_me = request.POST.get('remember_me')
 
         try:
             user = User.objects.get(username=username)
@@ -26,8 +29,15 @@ def loginUser(request):
         user = authenticate(request, username=username, password=password)
 
         if user is not None:
+            if remember_me:
+                request.session.set_expiry(settings.SESSION_COOKIE_AGE)
+            else:
+                request.session.set_expiry(0)
             login(request, user)
-            return redirect('home')
+            if valuenext == '':
+                return redirect('home')
+            else:
+                return redirect(valuenext)
         else:
             messages.error(request, 'Username or password does not exist')
 
@@ -54,7 +64,6 @@ def registerUser(request):
             login(request, user)
             return redirect('home')
         else:
-            print(form.errors)
             messages.error(request, 'An error occurred during registration')
 
     context = {'page': page, 'form': form}
